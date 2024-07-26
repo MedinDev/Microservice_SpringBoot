1a00639f183987a430984fb91467ac54575e3108
Medtex <medtexprog@gmail.com>
2024-07-26 21:09:18 +0800

    day 4

--- accounts/src/main/java/com/medinDev/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/medinDev/accounts/dto/CustomerDto.java
@@ -1,11 +1,24 @@
 package com.medinDev.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
+
+    @NotEmpty(message = "Name cannot be null or empty")
+    @Size(min = 5, max = 30, message = "Name should have at least 5 to 30 characters")
     private String name;
+
+    @NotEmpty(message = "Email address cannot be null or empty")
+    @Email(message = "Email address should be valid")
     private String email;
+
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be 10 digits")
     private String mobileNumber;
+
     private AccountsDto accountsDto;
 }
